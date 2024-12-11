ef66900376e026a96b8b9e918e0c783435b6f74b
Vishal Kotcherlakota <vkotcherlakota@nerdwallet.com>
2024-12-11 00:49:24 -0800

    2024 day 2 completed

--- aoc2024/solution/day02/day02.py
+++ aoc2024/solution/day02/day02.py
@@ -1,4 +1,5 @@
 from itertools import pairwise
+from collections.abc import Callable, Iterator
 
 
 def main():
@@ -6,34 +7,64 @@ def main():
         lines = f.readlines()
 
     print(f"Part 1:{part1(lines)}")
+    print(f"Part 2:{part2(lines)}")
 
 
-def is_monotonically_decreasing(input: list[int]) -> bool:
-    return all(x > y for x, y in pairwise(input))
+def all_pairs_are(f: Callable[[int, int], bool], input: list[int]) -> bool:
+    return all(f(x, y) for x, y in pairwise(input))
 
 
-def is_monotonically_increasing(input: list[int]) -> bool:
-    return all(x < y for x, y in pairwise(input))
+def all_but_one_pair_are(f: Callable[[int, int], bool], input: list[int]) -> bool:
+    i = iter(pairwise(input))
+    return any(not f(x, y) for x, y in i) and not any(not f(x, y) for x, y in i)
 
 
-def is_monotonic(input: list[int]) -> bool:
-    return is_monotonically_decreasing(input) or is_monotonically_increasing(input)
+def all_but_at_most_one_pair(f: Callable[[int, int], bool], input: list[int]) -> bool:
+    return all_pairs_are(f, input) or all_but_one_pair_are(f, input)
 
 
-def no_big_jumps(input: list[int]) -> bool:
-    r = [abs(x - y) < 3 for x, y in pairwise(input)]
-    return all(abs(x - y) <= 3 for x, y in pairwise(input))
+def increasing(x: int, y: int) -> bool:
+    return x < y
 
 
-def is_safe_report(report: str) -> bool:
-    vals = list(map(int, report.split(" ")))
-    return is_monotonic(vals) and no_big_jumps(vals)
+def decreasing(x: int, y: int) -> bool:
+    return x > y
+
+
+def no_big_jumps(x: int, y: int) -> bool:
+    return abs(x - y) <= 3
+
+
+def is_safe_report(vals: list[int]) -> bool:
+    return (
+        all_pairs_are(increasing, vals) or all_pairs_are(decreasing, vals)
+    ) and all_pairs_are(no_big_jumps, vals)
+
+
+def is_safe_enough_report(vals: list[int]) -> bool:
+    return is_safe_report(vals) or any(is_safe_report(r) for r in sublists(vals))
+
+
+def sublists(input: list[int]) -> Iterator[list[int]]:
+    for i in range(len(input)):
+        n = input.copy()
+        n.pop(i)
+        yield (n)
 
 
 def part1(input: list[str]) -> int:
     acc = 0
     for report in input:
-        acc += 1 if is_safe_report(report) else 0
+        vals = list(map(int, report.split(" ")))
+        acc += 1 if is_safe_report(vals) else 0
+    return acc
+
+
+def part2(input: list[str]) -> int:
+    acc = 0
+    for report in input:
+        vals = list(map(int, report.split(" ")))
+        acc += 1 if is_safe_enough_report(vals) else 0
     return acc
 
 
--- aoc2024/solution/day02/test_day02.py
+++ aoc2024/solution/day02/test_day02.py
@@ -1,5 +1,5 @@
 import pytest
-from day02 import is_safe_report
+from day02 import is_safe_report, is_safe_enough_report
 
 
 @pytest.mark.parametrize(
@@ -14,4 +14,21 @@ from day02 import is_safe_report
     ],
 )
 def test_is_safe_report(report, result):
-    assert result == is_safe_report(report)
+    vals = list(map(int, report.split(" ")))
+    assert result == is_safe_report(vals)
+
+
+@pytest.mark.parametrize(
+    "report,result",
+    [
+        ["7 6 4 2 1", True],
+        ["1 2 7 8 9", False],
+        ["9 7 6 2 1", False],
+        ["1 3 2 4 5", True],
+        ["8 6 4 4 1", True],
+        ["1 3 6 7 9", True],
+    ],
+)
+def test_is_safe_enough_report(report, result):
+    vals = list(map(int, report.split(" ")))
+    assert result == is_safe_enough_report(vals)
